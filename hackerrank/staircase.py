25ba45dc33e63b0617d166ff5db3f024de5163b8
Fidelmar Coronel <36834979+coronel08@users.noreply.github.com>
2021-10-09 11:20:16 -0700

    staircase loop

---
+++ hackerrank/staircase.py
@@ -0,0 +1,28 @@
+"""
+Staircase detail
+
+This is a staircase of size 6:
+
+     #
+    ##
+   ###
+  ####
+ #####
+######
+"""
+
+
+# def staircase(n):
+#     for i in range(n):
+#         blanks = ' ' * (n-i-1)
+#         hashtag = '#'* (i+1)
+#         print(f'{blanks + hashtag}')
+
+// Refractored
+def staircase(n):
+    for i in range(n):
+        print(' '*(n-i-1) + '#'*(i+1) )
+
+if __name__ == '__main__':
+    n=6
+    staircase(n)
