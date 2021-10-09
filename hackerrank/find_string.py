0333ca0e82161cd05511d2911cd21e30f14be081
Fidelmar Coronel <36834979+coronel08@users.noreply.github.com>
2021-10-09 03:18:44 -0700

    Update find_string.py

--- hackerrank/find_string.py
+++ hackerrank/find_string.py
@@ -1,4 +1,5 @@
 """
+https://www.hackerrank.com/challenges/find-a-string/problem
 You have to print the number of times that the substring occurs in the given string. String traversal will take place from left to right, not from right to left.
 
 NOTE: String letters are case-sensitive.
