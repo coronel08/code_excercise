5086eeb8b22e9f6c70568d1819124f0ebc7109d9
Fidelmar Coronel <36834979+coronel08@users.noreply.github.com>
2021-10-21 11:15:25 -0700

    Update hackerrank_migratory_birds.py

--- hackerrank/hackerrank_migratory_birds.py
+++ hackerrank/hackerrank_migratory_birds.py
@@ -25,4 +25,21 @@ print(migratoryBirds(arr))
 Javascript Solution
 
 
+
+
+arr = [1,1,2,2,3]
+
+function migratoryBirds(arr){
+    counts = {}
+    arr.forEach((i) => counts[i] = (counts[i] || 0) +1)
+    console.log(counts)
+    maxValue = Object.values(counts).reduce((cur,prev) => cur > prev ? cur:prev)
+    filteredKeys = Object.keys(counts).find((key) => counts[key] == maxValue)
+    // above returns first match, below returns all matches
+    // filteredKeys = Object.keys(counts).filter((key) => counts[key] == maxValue)
+
+    return filteredKeys
+}
+    
+migratoryBirds(arr)
 """
